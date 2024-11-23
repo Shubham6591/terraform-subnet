4cca5b61f98e787c6432dde3acae0c7237242d86
Shubham6591 <shubhamgupta6591@gmail.com>
2024-11-23 11:30:19 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "vpc_id" {
+ description = "ID of the VPC where the subnet will be created"
+ type = string
+
+}
+variable "cidr_block" {
+ description = "CIDR block for the subnet"
+ type = string
+
+}
+variable "subnet_name" {
+ description = "Name of the subnet"
+ type = string
+
+}
